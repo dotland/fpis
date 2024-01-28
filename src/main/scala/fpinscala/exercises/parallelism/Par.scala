65d9fc77ae6fe2a25b5f03803492b739bdf29f51
dotland <tisarukhan@gmail.com>
2024-01-28 21:11:37 +0400

    Solution 6.11 simulate state machine

--- src/main/scala/fpinscala/exercises/parallelism/Par.scala
+++ src/main/scala/fpinscala/exercises/parallelism/Par.scala
@@ -11,9 +11,9 @@ object Par:
     es => UnitFuture(a) // `unit` is represented as a function that returns a `UnitFuture`, which is a simple implementation of `Future` that just wraps a constant value. It doesn't use the `ExecutorService` at all. It's always done and can't be cancelled. Its `get` method simply returns the value that we gave it.
 
   private case class UnitFuture[A](get: A) extends Future[A]:
-    def isDone = true
-    def get(timeout: Long, units: TimeUnit) = get
-    def isCancelled = false
+    def isDone: Boolean = true
+    def get(timeout: Long, units: TimeUnit): A = get
+    def isCancelled: Boolean = false
     def cancel(evenIfRunning: Boolean): Boolean = false
 
   extension [A](pa: Par[A]) def map2[B, C](pb: Par[B])(f: (A, B) => C): Par[C] = // `map2` doesn't evaluate the call to `f` in a separate logical thread, in accord with our design choice of having `fork` be the sole function in the API for controlling parallelism. We can always do `fork(map2(a,b)(f))` if we want the evaluation of `f` to occur in a separate thread.
@@ -28,10 +28,10 @@ object Par:
       private val futureB = pb(es)
       @volatile private var cache: Option[C] = None
 
-      def isDone = cache.isDefined
-      def get() = get(Long.MaxValue, TimeUnit.NANOSECONDS)
+      def isDone: Boolean = cache.isDefined
+      def get(): C = get(Long.MaxValue, TimeUnit.NANOSECONDS)
 
-      def get(timeout: Long, units: TimeUnit) =
+      def get(timeout: Long, units: TimeUnit): C =
         val timeoutNanos = TimeUnit.NANOSECONDS.convert(timeout, units)
         val started = System.nanoTime
         val a = futureA.get(timeoutNanos, TimeUnit.NANOSECONDS)
@@ -41,12 +41,12 @@ object Par:
         cache = Some(c)
         c
 
-      def isCancelled = futureA.isCancelled || futureB.isCancelled
-      def cancel(evenIfRunning: Boolean) =
+      def isCancelled: Boolean = futureA.isCancelled || futureB.isCancelled
+      def cancel(evenIfRunning: Boolean): Boolean =
         futureA.cancel(evenIfRunning) || futureB.cancel(evenIfRunning)
 
   def fork[A](a: => Par[A]): Par[A] = // This is the simplest and most natural implementation of `fork`, but there are some problems with it--for one, the outer `Callable` will block waiting for the "inner" task to complete. Since this blocking occupies a thread in our thread pool, or whatever resource backs the `ExecutorService`, this implies that we're losing out on some potential parallelism. Essentially, we're using two threads when one should suffice. This is a symptom of a more serious problem with the implementation, and we will discuss this later in the chapter.
-    es => es.submit(new Callable[A] { def call = a(es).get })
+    es => es.submit(new Callable[A] { def call: A = a(es).get })
 
   def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
 
@@ -56,7 +56,7 @@ object Par:
   extension [A](pa: Par[A]) def map[B](f: A => B): Par[B] =
     pa.map2(unit(()))((a, _) => f(a))
 
-  def sortPar(parList: Par[List[Int]]) =
+  def sortPar(parList: Par[List[Int]]): Par[List[Int]] =
     parList.map(_.sorted)
 
   def sequenceSimple[A](pas: List[Par[A]]): Par[List[A]] =
@@ -84,8 +84,8 @@ object Par:
   def sequence[A](pas: List[Par[A]]): Par[List[A]] =
     sequenceBalanced(pas.toIndexedSeq).map(_.toList)
 
-  def parMap[A, B](ps: List[A])(f: A => B): Par[List[B]] = fork:
-    val fbs: List[Par[B]] = ps.map(asyncF(f))
+  def parMap[A, B](as: List[A])(f: A => B): Par[List[B]] = fork:
+    val fbs: List[Par[B]] = as.map(asyncF(f))
     sequence(fbs)
 
   def parFilter[A](l: List[A])(f: A => Boolean): Par[List[A]] = fork:
